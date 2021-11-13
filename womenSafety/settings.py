2f189aae5b8ee2f121b78653c54295eee778ca13
Dev Nirwal <devnirwal16@gmail.com>
2021-11-13 19:00:37 +0530

    he

--- womenSafety/settings.py
+++ womenSafety/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = "static_root"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
