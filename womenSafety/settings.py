3327fcae6c8ecc064beb8459d28dff89c4aeb384
Dev Nirwal <devnirwal16@gmail.com>
2021-11-13 18:56:47 +0530

    he

--- womenSafety/settings.py
+++ womenSafety/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     # local apps
     'safety',
